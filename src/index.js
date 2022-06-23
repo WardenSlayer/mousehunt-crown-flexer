42f8a9b6b274d105e1b2447ede480855f99e5e9f
Ben Loll <xTheCableGuyx@gmail.com>
2022-06-23 15:36:57 -0400

    Mobile Image Option added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import "./fonts/Baskerville-Semibold.ttf";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
